1411118b8a55d9cbf4f7f55e34dfc93e4a790b05
Mikael Tonnberg <mikael@carboncloud.io>
2021-05-23 18:56:50 +0200

    Some cleanup to show a more real case scenario

---
+++ src/Animals.elm
@@ -0,0 +1,45 @@
+module Animals exposing (AllKnownAnimals, addNewKnownAnimal, initAllKnownAnimals, lookupRealAnimal)
+
+import Dict exposing (Dict)
+import RefinementProofs.Knowledge exposing (A, NoDomainKnowledge, NoNamedKnowledge, NoValueKnowledge, WithKnowledge, axiomaticallySetDomainKnowledge, forget, setNamedKnowledge, withName, withNoKnowledge)
+import RefinementProofs.Proofs.DictProofs exposing (IsInDict, proveKeyIsInDict, takeValueFromDict)
+import RefinementProofs.Proofs.NumberProofs exposing (Positive, provePositive)
+
+
+type AllKnownAnimals
+    = AllKnownAnimals
+
+
+initAllKnownAnimals : WithKnowledge (Dict Int String) NoValueKnowledge AllKnownAnimals NoNamedKnowledge
+initAllKnownAnimals =
+    axiomaticallySetDomainKnowledge AllKnownAnimals <| withNoKnowledge <| Dict.fromList [ ( 1, "Wombat" ), ( 2, "Unicorn" ), ( -1, "Imaginary horse" ) ]
+
+
+addNewKnownAnimal :
+    Int
+    -> String
+    -> WithKnowledge (Dict Int String) NoValueKnowledge AllKnownAnimals NoNamedKnowledge
+    -> WithKnowledge (Dict Int String) NoValueKnowledge AllKnownAnimals NoNamedKnowledge
+addNewKnownAnimal animalId animalName animals =
+    axiomaticallySetDomainKnowledge AllKnownAnimals <|
+        withNoKnowledge <|
+            Dict.insert animalId animalName <|
+                forget animals
+
+
+lookupRealAnimal :
+    WithKnowledge (A Int animalId) NoValueKnowledge NoDomainKnowledge NoNamedKnowledge
+    -> WithKnowledge (A (Dict Int String) animals) NoValueKnowledge AllKnownAnimals NoNamedKnowledge
+    -> Maybe (WithKnowledge (A Int animalId) Positive NoDomainKnowledge (IsInDict animalId animals))
+lookupRealAnimal namedWantedKey namedDict =
+    case ( proveKeyIsInDict (forget namedWantedKey) (forget namedDict), withName provePositive (forget namedWantedKey) ) of
+        ( Just isInDictProof, Just isPositive ) ->
+            let
+                keyWithAllNeededProofs : WithKnowledge (A Int animalId) Positive NoDomainKnowledge (IsInDict animalId animals)
+                keyWithAllNeededProofs =
+                    setNamedKnowledge isInDictProof isPositive
+            in
+            Just keyWithAllNeededProofs
+
+        _ ->
+            Nothing
--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,15 @@
 module Main exposing (main)
 
+import Animals exposing (AllKnownAnimals, addNewKnownAnimal, initAllKnownAnimals, lookupRealAnimal)
 import Browser
+import CuteAnimals exposing (IsAValidCuteAnimal, proveCuteness)
 import Dict exposing (Dict)
 import Html exposing (Html, button, pre, text)
 import Html.Events exposing (onClick)
 import Http
 import RefinementProofs.Knowledge exposing (A, NoDomainKnowledge, NoNamedKnowledge, NoValueKnowledge, Proof, WithKnowledge, axiomaticallySetDomainKnowledge, forget, forgetNamedKnowledge, forgetNamedKnowledgeAndName, name, name2, name2WithKnowledge, raw, setNamedKnowledge, withName, withNoKnowledge)
-import RefinementProofs.Proofs.DictProofs exposing (IsInDict, proveKeyIsInDict)
+import RefinementProofs.Proofs.DictProofs exposing (IsInDict, proveKeyIsInDict, takeValueFromDict)
 import RefinementProofs.Proofs.NumberProofs exposing (Positive, provePositive)
-import RefinementProofs.Proofs.StringProofs exposing (NonEmptyString, proveNonEmptyString)
-import Dict
-import RefinementProofs.Proofs.DictProofs exposing (takeValueFromDict)
 
 
 
@@ -30,16 +29,6 @@ main =
 -- MODEL
 
 
---Should be in a separate module to protect ctor
-type IsAValidCuteAnimal
-    = IsAValidCuteAnimal
-
-
---Should be in a separate module to protect ctor
-type AllKnownAnimals
-    = AllKnownAnimals
-
-
 type alias Model =
     { animals : WithKnowledge (Dict Int String) NoValueKnowledge AllKnownAnimals NoNamedKnowledge
     , selectedAnimalId : Maybe (WithKnowledge Int Positive IsAValidCuteAnimal NoNamedKnowledge)
@@ -48,7 +37,7 @@ type alias Model =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { animals = axiomaticallySetDomainKnowledge AllKnownAnimals <| withNoKnowledge <| Dict.fromList [ ( 1, "Wombat" ), ( 2, "Unicorn" ), (-1, "Imaginary horse")]
+    ( { animals = initAllKnownAnimals
       , selectedAnimalId = Nothing
       }
     , Cmd.none
@@ -70,20 +59,13 @@ update msg model =
         SetCuteAnimal i ->
             case Maybe.map forgetNamedKnowledgeAndName <| name2WithKnowledge (withNoKnowledge i) model.animals handleSetCuteAnimal of
                 Just newId ->
-                    ( { model | selectedAnimalId = Just newId }
-                    , Cmd.none
-                    )
+                    ( { model | selectedAnimalId = Just newId }, Cmd.none )
 
                 Nothing ->
                     ( model, Cmd.none )
+
         AddToKnownAnimals animalId animalName ->
-          -- This should be in another module to hide ctors and important logic
-          let
-            newKnownAnimals = axiomaticallySetDomainKnowledge AllKnownAnimals <| withNoKnowledge <|
-              Dict.insert animalId animalName
-              <| forget model.animals
-          in
-          ( { model | animals = newKnownAnimals }, Cmd.none )
+            ( { model | animals = addNewKnownAnimal animalId animalName model.animals }, Cmd.none )
 
 
 handleSetCuteAnimal :
@@ -91,41 +73,9 @@ handleSetCuteAnimal :
     -> WithKnowledge (A (Dict Int String) animals) NoValueKnowledge AllKnownAnimals NoNamedKnowledge
     -> Maybe (WithKnowledge (A Int animalId) Positive IsAValidCuteAnimal NoNamedKnowledge)
 handleSetCuteAnimal namedId namedDict =
-    lookupRealAnimal namedId (forget namedDict) |>
-    Maybe.andThen (ensureCuteness namedDict) |>
-    Maybe.map forgetNamedKnowledge
-
-
-lookupRealAnimal :
-    WithKnowledge (A Int animalId) NoValueKnowledge NoDomainKnowledge NoNamedKnowledge
-    -> A (Dict Int String) animals
-    -> Maybe (WithKnowledge (A Int animalId) Positive NoDomainKnowledge (IsInDict animalId animals))
-lookupRealAnimal namedWantedKey namedDict =
-    case ( proveKeyIsInDict (forget namedWantedKey) namedDict, withName provePositive (forget namedWantedKey) ) of
-        ( Just isInDictProof, Just isPositive ) ->
-            let
-                keyWithAllNeededProofs : WithKnowledge (A Int animalId) Positive NoDomainKnowledge (IsInDict animalId animals)
-                keyWithAllNeededProofs =
-                    setNamedKnowledge isInDictProof isPositive
-            in
-            Just keyWithAllNeededProofs
-
-        _ ->
-            Nothing
-
-ensureCuteness :
-    WithKnowledge (A (Dict Int String) animals) NoValueKnowledge AllKnownAnimals NoNamedKnowledge
-    -> WithKnowledge (A Int animalId) Positive NoDomainKnowledge (IsInDict animalId animals)
-    -> Maybe (WithKnowledge (A Int animalId) Positive IsAValidCuteAnimal NoNamedKnowledge)
-ensureCuteness allAnimals animalId =
-  let
-    animalCommonName = takeValueFromDict animalId (forget allAnimals)
-  in
-  -- No animals with q can ever be cute, nor any animals with an id over 40, that is just common sense
-  if String.contains "q" animalCommonName || raw animalId > 40 then
-    Nothing
-  else
-    Just <| axiomaticallySetDomainKnowledge IsAValidCuteAnimal <| forgetNamedKnowledge animalId
+    lookupRealAnimal namedId namedDict
+        |> Maybe.andThen (proveCuteness namedDict)
+        |> Maybe.map forgetNamedKnowledge
 
 
 
@@ -160,7 +110,7 @@ view model =
         , text "\n\n"
         , button [ onClick (SetCuteAnimal 10) ] [ text "set animal to 90 (should not work - even if it is added - since oozing death monster is not a cute animal,decided by domain logic)" ]
         , text "\n\n"
-        , text <| "Known animals: " ++ (Debug.toString model.animals) ++ "\n\n"
+        , text <| "Known animals: " ++ Debug.toString model.animals ++ "\n\n"
         , button [ onClick (AddToKnownAnimals 10 "Ticklemonster") ] [ text "Add Ticklemonster, id 10 to the list of known animals" ]
         , text "\n\n"
         , button [ onClick (AddToKnownAnimals 90 "Oozing death monster") ] [ text "Add Oozing death monster, id 90 to the list of known animals" ]
